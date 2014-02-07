5a2dada38a8ee467e4632ff3a23cf6aa672a68b1
Pieter Omvlee <mail@pieteromvlee.net>
2014-02-07 11:55:58 +0100

    Popover fix

---
+++ BCPopover/BCPopoverContentController.h
@@ -0,0 +1,12 @@
+// Created by Pieter Omvlee on 07/02/2014.
+
+#import <Foundation/Foundation.h>
+
+@class BCPopover;
+
+@protocol BCPopoverContentController <NSObject>
+
+@optional;
+- (void)popoverWindowDidShow:(BCPopover *)popover;
+
+@end
